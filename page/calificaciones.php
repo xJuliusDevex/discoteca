bc293e7e957b19980e7c2f0aa0be983b27dd945d
xJuliusDevex <jcesargarq22@gmail.com>
2022-08-22 18:00:30 -0500

    carta

--- page/calificaciones.php
+++ page/calificaciones.php
@@ -16,7 +16,7 @@ include('../class/cliente.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dejanos tu comentario</title>
+    <title>paradise</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/ddc011add7.js" crossorigin="anonymous"></script>
--- page/carta.php
+++ page/carta.php
@@ -16,16 +16,21 @@ include('../class/cliente.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dejanos tu comentario</title>
+    <title>paradise</title>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Open+Sans&display=swap" rel="stylesheet"> 
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.1/normalize.min.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glider-js@1.7.3/glider.min.css">
+	<link rel="stylesheet" href="css/estilos.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/ddc011add7.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glider-js@1.7.7/glider.min.css">
-    <link rel="stylesheet" href="../style/style.css">
-    <link rel="stylesheet" href="comentario_styles.css">
+    
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=DynaPuff&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="../style/style.css">
+    <link rel="stylesheet" href="../style/comentario_styles.css">
 </head>
 <body class="contenedor none">
 <header class="header">
@@ -50,8 +55,117 @@ include('../class/cliente.php');
         </nav>
     </header>
     <main class="main">
-    <p>carta imaginaria 
-    </p>
+    <center>
+				<div class="titulo_carta">
+					<p>
+					  CARTA
+					</p>
+				</div>
+				<div class="titulo_categorias">
+					<p>
+					  CATEGORIAS
+					</p>
+				</div>
+			
+				<div class="botones_categorias">
+
+					
+					
+					
+
+					<a href="https://www.google.com">
+						<div>
+							<button class="btn1">
+								Cocteles
+							</button>
+						</div>
+					</a>
+
+
+					
+
+					<a href="https://www.youtube.com/">
+						<div>
+							<button class="btn2">
+								Selladas
+							</button>
+						</div>
+					</a>
+
+
+					
+					<a href="https://www.google.com">
+						<div>
+							<button class="btn3">
+								Cervezas
+							</button>
+						</div>
+					</a>
+
+
+					<a href="https://www.google.com">
+						<div>
+							<button class="btn4">
+								Jarras
+							</button>
+						</div>
+					</a>
+
+
+					
+				</div>
+			</center>
+			<br>
+		<br>
+
+		<div class="carousel">
+			<div class="carousel__contenedor">
+				<button aria-label="Anterior" class="carousel__anterior">
+					<i class="fas fa-chevron-left"></i>
+				</button>
+
+				<div class="carousel__lista">
+					<a href="https://www.google.com">
+						<div class="carousel__elemento">
+							<img src="../img/producto/cocteles260px.jpg" alt="Rock and Roll Hall of Fame">
+							<p>Cocteles</p>
+						</div>
+					</a>
+					
+					<a href="https://www.google.com">
+						<div class="carousel__elemento">
+							<img src="../img/producto/cervezas260px.jpg" alt="Rock and Roll Hall of Fame">
+							<p>Cervezas</p>
+						</div>
+					</a>
+					<a href="https://www.google.com">
+						<div class="carousel__elemento">
+							<img src="../img/producto/tragos_Selladas.jpg" alt="Rock and Roll Hall of Fame">
+							<p>Selladas</p>
+						</div>
+					</a>
+					<a href="https://www.google.com">
+						<div class="carousel__elemento">
+							<img src="../img/producto/jarras.jpg" alt="Rock and Roll Hall of Fame">
+							<p>Jarras</p>
+						</div>
+					</a>
+	
+					
+				</div>
+
+				<button aria-label="Siguiente" class="carousel__siguiente">
+					<i class="fas fa-chevron-right"></i>
+				</button>
+			</div>
+
+			<div role="tablist" class="carousel__indicadores"></div>
+		</div>
+	</div>
+
+	<script src="https://cdn.jsdelivr.net/npm/glider-js@1.7.3/glider.min.js"></script>
+	<script src="https://kit.fontawesome.com/2c36e9b7b1.js" crossorigin="anonymous"></script>
+	<script src="../js/app.js"></script>
     <?php
     if(isset($_SESSION['id_cliente'])): ?>
     <div class="modal" tabindex="-1" id="miperfil" aria-labelledby="exampleModalLabel" aria-hidden="true">
